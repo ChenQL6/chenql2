098a9842769c92bfb4f95b9663dc6ef41bcaa50a
ChenQL6 <1571590774@qq.com>
2020-05-10 09:48:23 +0800

    第一周实验

---
+++ 实验1/实验1/实验1.cpp
@@ -0,0 +1,25 @@
+// 实验1.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+class point
+{
+public:
+	int x;
+	int y;
+	void output()
+	{
+		cout << x << endl << y << endl;
+	}
+};
+
+void main()
+{
+	point pt;
+	pt.output();
+
+}
+
