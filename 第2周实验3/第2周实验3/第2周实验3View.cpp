a6e9e15fcb298b4229e611815cb6ed129ff40ec2
ChenQL6 <1571590774@qq.com>
2020-05-10 12:01:37 +0800

    作以客户区中心为圆心的椭圆，并使椭圆与边界相切

---
+++ 第2周实验3/第2周实验3/第2周实验3View.cpp
@@ -0,0 +1,110 @@
+
+// 第2周实验3View.cpp : C第2周实验3View 类的实现
+//
+
+#include "stdafx.h"
+// SHARED_HANDLERS 可以在实现预览、缩略图和搜索筛选器句柄的
+// ATL 项目中进行定义，并允许与该项目共享文档代码。
+#ifndef SHARED_HANDLERS
+#include "第2周实验3.h"
+#endif
+
+#include "第2周实验3Doc.h"
+#include "第2周实验3View.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// C第2周实验3View
+
+IMPLEMENT_DYNCREATE(C第2周实验3View, CView)
+
+BEGIN_MESSAGE_MAP(C第2周实验3View, CView)
+	// 标准打印命令
+	ON_COMMAND(ID_FILE_PRINT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_DIRECT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_PREVIEW, &CView::OnFilePrintPreview)
+END_MESSAGE_MAP()
+
+// C第2周实验3View 构造/析构
+
+C第2周实验3View::C第2周实验3View()
+{
+	// TODO: 在此处添加构造代码
+
+}
+
+C第2周实验3View::~C第2周实验3View()
+{
+}
+
+BOOL C第2周实验3View::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: 在此处通过修改
+	//  CREATESTRUCT cs 来修改窗口类或样式
+
+	return CView::PreCreateWindow(cs);
+}
+
+// C第2周实验3View 绘制
+
+void C第2周实验3View::OnDraw(CDC* pDC)
+{
+	C第2周实验3Doc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+
+	// TODO: 在此处为本机数据添加绘制代码
+	int r;
+	CRect cr;
+	CClientDC dc(this);
+	this->GetClientRect(&cr);
+	dc.Ellipse(cr);
+
+}
+
+
+// C第2周实验3View 打印
+
+BOOL C第2周实验3View::OnPreparePrinting(CPrintInfo* pInfo)
+{
+	// 默认准备
+	return DoPreparePrinting(pInfo);
+}
+
+void C第2周实验3View::OnBeginPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 添加额外的打印前进行的初始化过程
+}
+
+void C第2周实验3View::OnEndPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 添加打印后进行的清理过程
+}
+
+
+// C第2周实验3View 诊断
+
+#ifdef _DEBUG
+void C第2周实验3View::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void C第2周实验3View::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+C第2周实验3Doc* C第2周实验3View::GetDocument() const // 非调试版本是内联的
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(C第2周实验3Doc)));
+	return (C第2周实验3Doc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// C第2周实验3View 消息处理程序
